7ec53237b522c6d74b26c3b0c4117085f7a5a8d8
mruthulsgit <memruthu@gmail.com>
2022-08-31 03:53:14 +0530

    it will work better,No offece

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -7,13 +7,24 @@ the rules are mentioned below
 1.you can choose one among the 3 item(Rock,Paper,Scissor)
 2.Computer will also choose one among those by randomly
 3.rock can beat scissor,scissors can beat paper and rock can beat scissor''')
-print('\n\n Here the game begins\n 0 for scissor\t 1 for rock \t 2 for papers')
+print('\n\n Here the game begins\n 0 for rock\t 1 for scissors \t 2 for papers')
 a = int(input())
 com = random.randint(0,2)
-print(com)
-if a == 0 and com == 2:
-    print('you won')
-elif a > com:
-    print('you won')
+if a >=3 or a < 0:
+    print('you entered a invalid number')
 else:
-    print('tie')
+    items = ['Rock','Scissor','Paper']
+    print(f'you choose {items[a]}')
+    print(f'computer choose {items[com]}')
+    if a == 0 and com == 2:
+        print('you lose')
+    elif a == 2 and com == 0:
+        print('you won')
+    elif a < com:
+        print('you won')
+    elif a > com:
+        print('you lose')
+    else :
+        print('its a draw')
+        
+
